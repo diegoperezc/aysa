4ef5bd55b159a7d5a3b372478566b2ba6950c8a4
diegoperezc <57495612+diegoperezc@users.noreply.github.com>
2023-11-16 15:00:14 -0300

    336429 - Fuera de Alcance
    Se pidio una consulta adicional

---
+++ Aplicaciones/Factura/query.sql
@@ -0,0 +1,6 @@
+select changedate --este
+--statusdate, invoicedate, duedate, paiddate, enterdate, glpostdate, en_periododesde, en_periodohasta
+from INVOICE
+WHERE en_invoicenum = '0201-00403054'
+ --and ist.changedate <=  hasta
+ --AND ist.changedate >=  desde
